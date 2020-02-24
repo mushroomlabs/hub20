bc9bf04433922f7c525ef41f41d90d2cdf746018
Raphael Lullis <raphael@lullis.net>
2020-02-24 22:12:57 +0100

    raiden_contracts is missing an dependency.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="hub20",
     url="https://github.com/mushroomlabs/hub20",
     description=description,
-    version="0.0.1",
+    version="0.0.3",
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
