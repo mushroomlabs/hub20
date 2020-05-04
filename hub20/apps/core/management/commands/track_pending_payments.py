1d6130e37eb64c7d3e7093ce8e007a374433c91f
Raphael Lullis <raphael@lullis.net>
2020-05-04 16:53:12 +0200

    Turn the track_pending_payment command from a asyncio into a celery task

--- hub20/apps/core/management/commands/track_pending_payments.py
+++ hub20/apps/core/management/commands/track_pending_payments.py
@@ -1,82 +1,54 @@
-import asyncio
 import logging
+import time
+from concurrent.futures import TimeoutError
 
 from django.core.management.base import BaseCommand
-from ethtoken.abi import EIP20_ABI
 
-from hub20.apps.blockchain.models import make_web3
+from hub20.apps.blockchain.models import Transaction, make_web3
+from hub20.apps.core import tasks
 from hub20.apps.core.models import Wallet
-from hub20.apps.core.signals import blockchain_payment_sent
 from hub20.apps.ethereum_money.models import EthereumToken
 
 logger = logging.getLogger(__name__)
 SLEEP_INTERVAL = 3
 
 
-async def track_pending_transactions(w3):
+def track_pending_transactions(w3):
     chain_id = int(w3.net.version)
 
     tx_filter = w3.eth.filter("pending")
 
-    ETH = EthereumToken.ETH(chain_id)
-
     while True:
-        pending_transaction_data = [
-            w3.eth.getTransaction(tx) for tx in tx_filter.get_new_entries()
-        ]
-
-        pending_transactions = [tx_data for tx_data in pending_transaction_data if tx_data]
-
-        if not pending_transactions:
-            await asyncio.sleep(SLEEP_INTERVAL)
-            continue
-
-        logger.info(f"Checking {len(pending_transactions)} pending transaction(s)")
-
-        wallet_addresses = Wallet.objects.filter(paymentordermethod__isnull=False).values_list(
-            "account__address", flat=True
-        )
-
+        time.sleep(SLEEP_INTERVAL)
+        wallets = Wallet.objects.filter(paymentordermethod__isnull=False)
         tokens = EthereumToken.ERC20tokens.filter(
             chain=chain_id, paymentorder__payment_method__isnull=False
         )
 
-        eth_payments = [tx for tx in pending_transactions if tx.to in wallet_addresses]
+        # Convert the querysets to lists of addresses
+        wallet_addresses = wallets.values_list("account__address", flat=True)[::1]
+        token_addresses = tokens.values_list("address", flat=True)[::1]
 
-        token_payments = {
-            token: [tx for tx in pending_transactions if tx.to == token.address]
-            for token in tokens
-        }
+        if not wallet_addresses:
+            logger.info("No open payment order...")
+            continue
 
-        for token, txs in token_payments.items():
-            if not txs:
-                continue
+        try:
+            pending_entries = [entry.hex() for entry in tx_filter.get_new_entries()]
+            recorded_tx_hashes = Transaction.objects.filter(hash__in=pending_entries).values_list(
+                "hash", flat=True
+            )[::1]
+            for tx_hash in set(pending_entries) - set(recorded_tx_hashes):
+                address_list = ", ".join([str(a) for a in wallet_addresses])
+                logger.info(f"Checking {tx_hash} for blockchain transfers at {address_list}")
+                tasks.check_transaction_for_eth_transfer.delay(tx_hash, wallet_addresses)
 
-            contract = w3.eth.contract(abi=EIP20_ABI, address=token.address)
-            for tx in txs:
-                logger.info(f"Processing {token.code} transaction: {tx.hash.hex()}")
-                fn, args = contract.decode_function_input(tx.input)
-
-                # TODO: is this really the best way to identify the function?
-                if fn.function_identifier == contract.functions.transfer.function_identifier:
-                    recipient = args["_to"]
-                    transfer_amount = token.from_wei(args["_value"])
-
-                    blockchain_payment_sent.send(
-                        sender=EthereumToken,
-                        recipient=recipient,
-                        amount=transfer_amount,
-                        transaction_hash=tx.hash.hex(),
+                if token_addresses:
+                    tasks.check_transaction_for_erc20_transfer.delay(
+                        tx_hash, wallet_addresses, token_addresses
                     )
-
-        for tx in eth_payments:
-            logger.info(f"Processing ETH transfer: {tx.hash.hex()}")
-            blockchain_payment_sent.send(
-                sender=EthereumToken,
-                recipient=tx.to,
-                amount=ETH.from_wei(tx.value),
-                transaction_data=tx,
-            )
+        except TimeoutError:
+            logger.warn("Timeout error when getting new entries")
 
 
 class Command(BaseCommand):
@@ -84,8 +56,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         w3 = make_web3()
-        loop = asyncio.get_event_loop()
-        try:
-            loop.run_until_complete(track_pending_transactions(w3))
-        finally:
-            loop.close()
+        track_pending_transactions(w3)
--- hub20/apps/core/tasks.py
+++ hub20/apps/core/tasks.py
@@ -1,13 +1,21 @@
 import logging
+from concurrent.futures import TimeoutError
+from typing import List
 
 from asgiref.sync import async_to_sync
 from celery import shared_task
 from channels.layers import get_channel_layer
 from django.db import transaction
 from django.utils import timezone
+from ethtoken.abi import EIP20_ABI
+from web3.exceptions import TransactionNotFound
+
+from hub20.apps.blockchain.models import make_web3
+from hub20.apps.ethereum_money.models import EthereumToken
 
 from .consumers import CheckoutConsumer
 from .models import PaymentOrderEvent, PaymentOrderMethod, Transfer
+from .signals import blockchain_payment_sent
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +42,70 @@ def expire_payment_method(payment_method_id):
         if not order.is_finalized:
             order.events.create(status=PaymentOrderEvent.STATUS.expired)
         payment_method.delete()
+
+
+@shared_task
+def check_transaction_for_erc20_transfer(
+    tx_hash: str, wallet_addresses: List[str], token_addresses: List[str]
+):
+    w3 = make_web3()
+    chain_id = int(w3.net.version)
+
+    try:
+        tx = w3.eth.getTransaction(tx_hash)
+        tokens = EthereumToken.objects.filter(chain=chain_id, address__in=token_addresses)
+        for token in tokens:
+            logger.info(f"Processing {token.code} transaction: {tx_hash}")
+            contract = w3.eth.contract(abi=EIP20_ABI, address=token.address)
+            fn, args = contract.decode_function_input(tx.input)
+
+            # TODO: is this really the best way to identify the function?
+            if fn.function_identifier == contract.functions.transfer.function_identifier:
+                recipient = args["_to"]
+
+                if recipient in wallet_addresses:
+                    blockchain_payment_sent.send(
+                        sender=EthereumToken,
+                        recipient=recipient,
+                        amount=token.from_wei(args["_value"]),
+                        transaction_hash=tx_hash,
+                    )
+
+    except TimeoutError:
+        logger.error(f"Failed request to get or check {tx_hash}")
+    except TransactionNotFound:
+        logger.info(f"Tx {tx_hash} has not yet been mined")
+    except ValueError as exc:
+        logger.error(str(exc))
+    except Exception as exc:
+        logger.exception(exc)
+
+
+@shared_task
+def check_transaction_for_eth_transfer(tx_hash: str, wallet_addresses: List[str]):
+    w3 = make_web3()
+    chain_id = int(w3.net.version)
+
+    try:
+        tx = w3.eth.getTransaction(tx_hash)
+
+        if tx.to in wallet_addresses:
+            ETH = EthereumToken.ETH(chain_id)
+            blockchain_payment_sent.send(
+                sender=EthereumToken,
+                recipient=tx.to,
+                amount=ETH.from_wei(tx.value),
+                transaction_hash=tx_hash,
+            )
+
+    except TimeoutError:
+        logger.warn(f"Failed request to get {tx_hash}")
+    except TransactionNotFound:
+        logger.info(f"Tx {tx_hash} has not yet been mined")
+    except ValueError as exc:
+        logger.error(str(exc))
+    except Exception as exc:
+        logger.exception(exc)
 
 
 @shared_task
