8dfd2c6a8d395881e017666cd12de7a0bba6fe58
Raphael Lullis <raphael@lullis.net>
2020-09-16 12:25:49 +0200

    Integrate paper-dashboard and paper-kit2 (SCSS only) into frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My Vue.js App"/>
+  <div :class="{'nav-open': $sidebar.showSidebar}">
+    <notifications></notifications>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.vue-notifyjs.notifications {
+  .alert {
+    z-index: 10000;
+  }
+  .list-move {
+    transition: transform 0.3s, opacity 0.4s;
+  }
+  .list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .list-enter-active {
+    transition: transform 0.2s ease-in, opacity 0.4s ease-in;
+  }
+  .list-leave-active {
+    transition: transform 1s ease-out, opacity 0.4s ease-out;
+  }
+
+  .list-enter {
+    opacity: 0;
+    transform: scale(1.1);
+  }
+  .list-leave-to {
+    opacity: 0;
+    transform: scale(1.2, 0.7);
+  }
 }
 </style>
---
+++ frontend/src/pages/UserProfile/EditProfileForm.vue
@@ -0,0 +1,131 @@
+<template>
+  <card class="card" title="Edit Profile">
+    <div>
+      <form @submit.prevent>
+        <div class="row">
+          <div class="col-md-5">
+            <fg-input type="text"
+                      label="Company"
+                      :disabled="true"
+                      placeholder="Paper dashboard"
+                      v-model="user.company">
+            </fg-input>
+          </div>
+          <div class="col-md-3">
+
+            <fg-input type="text"
+                      label="Username"
+                      placeholder="Username"
+                      v-model="user.username">
+            </fg-input>
+          </div>
+          <div class="col-md-4">
+            <fg-input type="email"
+                      label="Username"
+                      placeholder="Email"
+                      v-model="user.email">
+            </fg-input>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <fg-input type="text"
+                      label="First Name"
+                      placeholder="First Name"
+                      v-model="user.firstName">
+            </fg-input>
+          </div>
+          <div class="col-md-6">
+            <fg-input type="text"
+                      label="Last Name"
+                      placeholder="Last Name"
+                      v-model="user.lastName">
+            </fg-input>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <fg-input type="text"
+                      label="Address"
+                      placeholder="Home Address"
+                      v-model="user.address">
+            </fg-input>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <fg-input type="text"
+                      label="City"
+                      placeholder="City"
+                      v-model="user.city">
+            </fg-input>
+          </div>
+          <div class="col-md-4">
+            <fg-input type="text"
+                      label="Country"
+                      placeholder="Country"
+                      v-model="user.country">
+            </fg-input>
+          </div>
+          <div class="col-md-4">
+            <fg-input type="number"
+                      label="Postal Code"
+                      placeholder="ZIP Code"
+                      v-model="user.postalCode">
+            </fg-input>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label>About Me</label>
+              <textarea rows="5" class="form-control border-input"
+                        placeholder="Here can be your description"
+                        v-model="user.aboutMe">
+
+              </textarea>
+            </div>
+          </div>
+        </div>
+        <div class="text-center">
+          <p-button type="info"
+                    round
+                    @click.native.prevent="updateProfile">
+            Update Profile
+          </p-button>
+        </div>
+        <div class="clearfix"></div>
+      </form>
+    </div>
+  </card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        company: "Paper Dashboard",
+        username: "michael23",
+        email: "",
+        firstName: "Chet",
+        lastName: "Faker",
+        address: "Melbourne, Australia",
+        city: "Melbourne",
+        postalCode: "",
+        aboutMe: `We must accept finite disappointment, but hold on to infinite hope.`
+      }
+    };
+  },
+  methods: {
+    updateProfile() {
+      alert("Your data: " + JSON.stringify(this.user));
+    }
+  }
+};
+</script>
+<style>
+</style>
