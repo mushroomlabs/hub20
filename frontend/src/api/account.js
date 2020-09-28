8bb2b3f33c5e28caee016465980ca5a2845c71c0
Raphael Lullis <raphael@lullis.net>
2020-09-29 01:25:18 +0200

    Add basic table of transaction history
    Implements related store and api endpoints.

--- frontend/src/api/account.js
+++ frontend/src/api/account.js
@@ -1,6 +1,12 @@
 import session from './session';
 
 export default {
+  getCredits() {
+    return session.get('/api/credits');
+  },
+  getDebits() {
+    return session.get('/api/debits');
+  },
   getBalances() {
     return session.get('/api/balances');
   },
--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -1,6 +1,60 @@
 <template>
-  <h1>History</h1>
+<div id="history">
+  <card title="Transactions">
+    <table class="table">
+      <thead>
+        <tr>
+          <td class="transaction-date">Date</td>
+          <td class="description">Description</td>
+          <td class="token">Token</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="transaction in transactions" :class="transaction.type" :key="transaction.id">
+          <td class="transaction-date">{{ new Date(transaction.created).toLocaleString() }}</td>
+          <td class="description">{{ transaction.summary }}</td>
+          <td class="amount">{{ transaction.amount }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </card>
+</div>
 </template>
+
 <script>
-  export default {}
+import {mapGetters} from "vuex";
+
+export default {
+  name: "history",
+  computed: {
+    ...mapGetters("account", ["transactions"])
+  }
+}
 </script>
+
+<style lang="scss">
+@import '../../node_modules/bootstrap/scss/bootstrap.scss';
+
+#history {
+    @include make-container();
+
+    table {
+        tbody {
+
+            tr.debit {
+                td.amount {
+                    &:before {
+                        content: "- ";
+                    }
+                    color: red;
+                }
+            }
+            tr.credit {
+                td.amount {
+                    color: green;
+                }
+            }
+        }
+    }
+}
+</style>
