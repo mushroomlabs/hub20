a7989903468b55c61ea7e42c8bd413002635d431
Raphael Lullis <raphael@lullis.net>
2020-10-15 23:07:18 +0200

    Add actions and mutations to edit store

--- frontend/src/store/stores.js
+++ frontend/src/store/stores.js
@@ -5,42 +5,57 @@ import {
   STORE_SETUP_BEGIN,
   STORE_SETUP_SUCCESS,
   STORE_SETUP_FAILURE,
+  STORE_SET_NAME,
+  STORE_SET_URL,
+  STORE_SET_ACCEPTED_TOKENS,
   STORE_UPDATE_BEGIN,
   STORE_UPDATE_SUCCESS,
   STORE_UPDATE_FAILURE,
-  STORE_CREATE_FAILURE
+  STORE_CREATE_FAILURE,
 } from './types'
 
 const initialState = {
   stores: {},
   editing: null,
   error: null,
+  loaded: false,
 }
 
 const actions = {
-  fetchStores({ commit }) {
-    return stores.getList()
-      .then(({ data }) => commit(STORE_COLLECTION_SET, data))
+  fetchStores({commit}) {
+    return stores
+      .getList()
+      .then(({data}) => commit(STORE_COLLECTION_SET, data))
       .then(() => commit(STORE_SETUP_SUCCESS))
       .catch((error) => commit(STORE_SETUP_FAILURE, error))
   },
-  updateStore({ commit }, storeData) {
-    return stores.update(storeData)
+  updateStore({commit}, storeData) {
+    return stores
+      .update(storeData)
       .then(() => commit(STORE_UPDATE_SUCCESS))
       .catch(() => commit(STORE_UPDATE_FAILURE))
   },
   createStore({commit, dispatch}, storeData) {
-    return stores.create(storeData)
+    return stores
+      .create(storeData)
       .then(() => dispatch('fetchStores'))
       .catch((error) => commit(STORE_CREATE_FAILURE, error))
   },
-  initialize({ commit, dispatch }) {
-    commit(STORE_SETUP_BEGIN)
-    dispatch("fetchStores")
+  editStore({state, commit}, storeId) {
+    const store = state.stores[storeId]
+    const storeDataPromise = store ? Promise.resolve({data: store}) : stores.get(storeId)
+
+    return storeDataPromise
+      .then(({data}) => commit(STORE_UPDATE_BEGIN, data))
+      .catch((error) => commit(STORE_UPDATE_FAILURE, error))
   },
-  refresh({ dispatch }) {
-    dispatch("fetchStores")
-  }
+  initialize({commit, dispatch}) {
+    commit(STORE_SETUP_BEGIN)
+    dispatch('fetchStores')
+  },
+  refresh({dispatch}) {
+    dispatch('fetchStores')
+  },
 }
 
 const mutations = {
@@ -52,18 +67,40 @@ const mutations = {
   },
   [STORE_SETUP_SUCCESS](state) {
     state.error = null
+    state.loaded = true
   },
-  [STORE_UPDATE_BEGIN](state, storeId) {
-    let storeData = state.stores[storeId]
+  [STORE_UPDATE_BEGIN](state, storeData) {
     state.editing = storeData
     state.error = null
+  },
+  [STORE_UPDATE_SUCCESS](state) {
+    state.editing = null
+    state.error = null
+  },
+  [STORE_UPDATE_FAILURE](state, error) {
+    state.error = error
+  },
+  [STORE_SET_NAME](state, name) {
+    if (state.editing) {
+      state.editing.name = name
+    }
+  },
+  [STORE_SET_URL](state, siteUrl) {
+    if (state.editing) {
+      state.editing.site_url = siteUrl
+    }
+  },
+  [STORE_SET_ACCEPTED_TOKENS](state, acceptedTokens) {
+    if (state.editing) {
+      state.editing.accepted_currencies = acceptedTokens
+    }
   },
   [STORE_COLLECTION_SET](state, data) {
     state.stores = data.reduce((acc, store) => Object.assign({[store.id]: store}, acc), {})
   },
   [STORE_CREATE_FAILURE](state, error) {
     state.error = error
-  }
+  },
 }
 
 export default {
--- frontend/src/views/StoreDetail.vue
+++ frontend/src/views/StoreDetail.vue
@@ -1,20 +1,60 @@
 <template>
 <div id="store-detail" v-if="store">
 
-  <fg-input label="Name" placeholder="Store Name" v-model="store.name" required/>
-  <fg-input label="URL" placeholder="Store URL" v-model="store.url" required/>
-
+  <fg-input label="Name" placeholder="Store Name" v-model="name" required/>
+  <fg-input label="URL" placeholder="Store URL" v-model="siteUrl" required/>
+  <fg-select label="Accepted Tokens" v-model="acceptedTokens" :options="tokenOptions" multiple />
 </div>
 </template>
 <script>
-import {mapState} from "vuex"
+import {mapState, mapGetters, mapMutations, mapActions} from "vuex"
 
 export default {
   computed: {
-    ...mapState("stores", {stores: "stores"}),
-    store() {
-      return this.stores[this.$route.params.id];
+    ...mapState("stores", {store: "editing", stores: "stores"}),
+    ...mapGetters("tokens", {tokenMap: "tokensByAddress"}),
+    tokenOptions() {
+      return Object.values(this.tokenMap).map((token) => {
+        return {
+          value: token.address,
+          text: token.code
+        }})
+    },
+    name: {
+      get() {
+        return this.store && this.store.name
+      },
+      set(value) {
+        return this.updateName(value)
+      }
+    },
+    acceptedTokens: {
+      get() {
+        return this.store && this.store.accepted_currencies
+      },
+      set(value) {
+        this.$store.commit("stores/STORE_SET_ACCEPTED_TOKENS", value)
+      }
+    },
+    siteUrl: {
+      get() {
+        return this.store && this.store.site_url
+      },
+      set(value) {
+        return this.updateSiteUrl(value)
+      }
     }
+  },
+  methods: {
+    ...mapMutations("stores", {
+      updateName: "STORE_SET_NAME",
+      updateSiteUrl: "STORE_SET_URL",
+      updateAcceptedTokens: "STORE_SET_ACCEPTED_TOKENS"
+    }),
+    ...mapActions("stores", ["editStore"])
+  },
+  mounted() {
+    this.editStore(this.$route.params.id)
   }
 }
 </script>
