c7eb3c74681595d1524452f948e68daa6980d3af
Raphael Lullis <raphael@lullis.net>
2020-10-20 03:07:29 +0200

    Error message and error state handling adapted for StoreDetail

--- frontend/src/views/StoreDetail.vue
+++ frontend/src/views/StoreDetail.vue
@@ -1,31 +1,98 @@
 <template>
 <div id="store-detail" v-if="store">
+  <card>
+    Stores are used when you want to have any website or payment
+    system that you like to receive payments on your behalf. For more
+    information about the checkout system and how to create payment
+    integrations, check the <a href="https://docs.hub20.io"
+    target="_blank">documentation</a> site.
+  </card>
 
-  <fg-input label="Name" placeholder="Store Name" v-model="name" required/>
-  <fg-input label="URL" placeholder="Store URL" v-model="siteUrl" required/>
-  <fg-select label="Accepted Tokens" v-model="acceptedTokens" :options="tokenOptions" multiple />
+  <fg-input v-if="store.id" label="Store ID" :value="store.id" addon-right-icon="ti-files" disabled />
+
+  <fg-input
+    :errorMessage="validationErrors.name"
+    label="Name"
+    placeholder="Store Name"
+    v-model="name"
+    required
+    />
+
+  <fg-input
+    label="URL"
+    placeholder="Store URL"
+    v-model="siteUrl"
+    :errorMessage="validationErrors.siteUrl"
+    required />
+
+  <fg-select
+    label="Accepted Tokens"
+    v-model="acceptedTokens"
+    :options="tokenOptions"
+    :errorMessage="validationErrors.acceptedTokens"
+    multiple
+    />
+
+  <fg-input v-if="store.public_key" label="Public Key" :value="store.public_key" addon-right-icon="ti-files" disabled/>
+
+  <p-button block @click.native="save(store)" :disabled="!isValid">Save</p-button>
 </div>
 </template>
 <script>
-import {mapState, mapGetters, mapMutations, mapActions} from "vuex"
+import {mapGetters, mapMutations, mapActions} from 'vuex'
 
 export default {
+  data() {
+    return {
+      validationErrors: {}
+    }
+  },
+  watch: {
+    name() {
+      if (!this.name.trim()) {
+        this.$set(this.validationErrors, 'name', 'Store Name can not be empty')
+      }
+      else {
+        this.$set(this.validationErrors, 'name', null)
+      }
+    },
+    siteUrl() {
+      if (!this.siteUrl.trim()) {
+        this.$set(this.validationErrors, 'siteUrl', 'Store must have a valid URL')
+      }
+      else {
+        this.$set(this.validationErrors, 'siteUrl', null)
+      }
+    },
+    acceptedTokens() {
+      if (!this.acceptedTokens.length) {
+        this.$set(this.validationErrors, 'acceptedTokens', 'Stores must use at least one token')
+      }
+      else {
+        this.$set(this.validationErrors, 'acceptedTokens', null)
+      }
+    },
+  },
   computed: {
-    ...mapState("stores", {store: "editing", stores: "stores"}),
-    ...mapGetters("tokens", {tokenMap: "tokensByAddress"}),
+    ...mapGetters('stores', {store: 'storeEditData', stores: 'storesById', submissionErrors: 'storeEditError'}),
+    ...mapGetters('tokens', {tokenMap: 'tokensByAddress'}),
+    isValid() {
+      return Object.values(this.validationErrors).every(attr => !attr)
+    },
     tokenOptions() {
       return Object.values(this.tokenMap).map((token) => {
         return {
           value: token.address,
-          text: token.code
-        }})
+          text: token.code,
+        }
+      })
     },
     name: {
       get() {
         return this.store && this.store.name
       },
       set(value) {
-        return this.updateName(value)
+        this.updateName(value.trim())
       }
     },
     acceptedTokens: {
@@ -33,8 +100,8 @@ export default {
         return this.store && this.store.accepted_currencies
       },
       set(value) {
-        this.$store.commit("stores/STORE_SET_ACCEPTED_TOKENS", value)
-      }
+        this.updateAcceptedTokens(value)
+      },
     },
     siteUrl: {
       get() {
@@ -42,19 +109,35 @@ export default {
       },
       set(value) {
         return this.updateSiteUrl(value)
-      }
-    }
+      },
+    },
   },
   methods: {
-    ...mapMutations("stores", {
-      updateName: "STORE_SET_NAME",
-      updateSiteUrl: "STORE_SET_URL",
-      updateAcceptedTokens: "STORE_SET_ACCEPTED_TOKENS"
+    ...mapMutations('stores', {
+      updateName: 'STORE_UPDATE_SET_NAME',
+      updateSiteUrl: 'STORE_UPDATE_SET_URL',
+      updateAcceptedTokens: 'STORE_UPDATE_SET_ACCEPTED_TOKENS',
     }),
-    ...mapActions("stores", ["editStore"])
+    ...mapActions('stores', ['editStore', 'updateStore', 'createStore']),
+    save(storeData) {
+      const action = storeData.id ? this.updateStore : this.createStore
+
+      action(storeData)
+    }
   },
   mounted() {
     this.editStore(this.$route.params.id)
-  }
+  },
 }
 </script>
+
+<style lang="scss">
+#store-detail {
+  div.form-group.input-group {
+    label {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
