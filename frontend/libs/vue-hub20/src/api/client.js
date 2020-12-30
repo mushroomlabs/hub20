895ce311c32f22f1e2ad00a65116a835dec944df
Raphael Lullis <raphael@lullis.net>
2020-12-30 17:34:36 +0100

    Use application/json by default on all axios client instances

--- frontend/libs/vue-hub20/src/api/client.js
+++ frontend/libs/vue-hub20/src/api/client.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
-const client = axios.create()
+const client = axios.create({
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
 
 export default client
